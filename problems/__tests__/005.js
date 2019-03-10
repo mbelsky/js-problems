e80f02b8a2aaea4f127a6b3af4db1ca6b6eed669
Maxim Belsky <public.belsky@gmail.com>
2019-03-10 21:50:23 +0300

    Add 005-union problem

---
+++ problems/__tests__/005.js
@@ -0,0 +1,9 @@
+const union = require('../005-union');
+
+test('Тестирование задачи "005-union"', () => {
+    expect(union([1, 1, 3, 2, 5], [5, 3, 7, 7])).toEqual([3, 5]);
+    expect(
+        union([2, 4, 6, 8, 10, 12, 10, 8, 6, 4, 2], [3, 6, 9, 12, 15, 18])
+    ).toEqual([6, 12]);
+    expect(union([1, 2, 3], [11, 0, -1])).toEqual([]);
+});
