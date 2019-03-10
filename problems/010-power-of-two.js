9eec6b961e3d46e34ea4b37a6a61a84c164b6eb1
Maxim Belsky <public.belsky@gmail.com>
2019-03-11 00:42:38 +0300

    Add 010-power-of-two problem

---
+++ problems/010-power-of-two.js
@@ -0,0 +1,18 @@
+/**
+ * Напишите функцию getPower(n) возвращающую челочисленное значение степени >= 0,
+ * в которую нужно возвести двойку, чтобы получить n
+ *
+ * Пример:
+ *
+ * getPower(2) === 1            # 2^1 = 2
+ * getPower(3) === undefined
+ * getPower(256) === 8          # 2^8 = 256
+ *
+ * @param {number} n
+ * @returns {number|undefined}
+ */
+function getPower(n) {
+    return -1;
+}
+
+module.exports = getPower;
