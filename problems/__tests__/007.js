5a1be3a7aca49cf2530dbdcb22e8cc9c5957f5ba
Maxim Belsky <public.belsky@gmail.com>
2019-03-10 23:55:12 +0300

    Add 007-bus-tour problem

---
+++ problems/__tests__/007.js
@@ -0,0 +1,6 @@
+const checkBusTour = require('../007-bus-tour');
+
+test('Тестирование задачи "007-bus-tour"', () => {
+    expect(checkBusTour(600, 1024)).toBeUndefined();
+    expect(checkBusTour(600, 512)).toBe(2);
+});
