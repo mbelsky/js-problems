f7b7d9e78af0771406adba6e2fa0359d8be5ef69
Maxim Belsky <public.belsky@gmail.com>
2019-03-10 18:59:10 +0300

    Add 001-sum problem

---
+++ problems/__tests__/001.js
@@ -0,0 +1,8 @@
+const sum = require('../001-sum');
+
+test('Тестирование задачи "001-sum"', () => {
+    expect(sum(-100)).toBe(1);
+    expect(sum(0)).toBe(1);
+    expect(sum(2)).toBe(3);
+    expect(sum(5)).toBe(15);
+});
