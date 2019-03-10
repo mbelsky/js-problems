5befc4beb5224d5c7636e05cfbe21ea014808d69
Maxim Belsky <public.belsky@gmail.com>
2019-03-11 00:28:28 +0300

    Add 009-time-delta problem

---
+++ problems/009-time-delta.js
@@ -0,0 +1,23 @@
+/**
+ * Напишите функцию getTimeDelta(x, y) определяющую,
+ * сколько секунд прошло между двумя моментами времени x и y.
+ *
+ * Время передается в формате 'HH:MM:SS'. Минимальное значение – '00:00:00', максимальное – '23:59:59'.
+ *
+ * По условию x всегда меньше y.
+ *
+ * Пример:
+ *
+ * getTimeDelta('00:00:00', '00:00:01') === 1
+ * getTimeDelta('01:01:01', '02:02:02') === 3661
+ * getTimeDelta('01:19:30', '01:20:20') === 50
+ *
+ * @param {string} x
+ * @param {string} y
+ * @returns {number} разница между x и y в секундах
+ */
+function getTimeDelta(x, y) {
+    return undefined;
+}
+
+module.exports = getTimeDelta;
