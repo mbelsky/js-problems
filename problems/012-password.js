b6bc0717eb1f6785237abd20f3d1db039b882d0e
Maxim Belsky <public.belsky@gmail.com>
2019-03-11 01:06:46 +0300

    Add 012-password problem

---
+++ problems/012-password.js
@@ -0,0 +1,24 @@
+/**
+ * Пароль называется криптостойким,
+ * если он включает в себя хотя бы одну строчную английскую букву,
+ * хотя бы одну заглавную английскую букву и хотя бы одну цифру,
+ * при этом его длина должна быть не менее 7 символов.
+ *
+ * Напишите функцию validatePassword(password) определяющую,
+ * является ли переданный пароль криптостойким.
+ *
+ * Пример:
+ *
+ * validatePassword('abc4DEFG') === true
+ * validatePassword('abcdefg') === false
+ * validatePassword('abcdefG') === false
+ * validatePassword('abcdef7') === false
+ *
+ * @param {string} password
+ * @returns {boolean}
+ */
+function validatePassword(password) {
+    return undefined;
+}
+
+module.exports = validatePassword;
