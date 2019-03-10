d8002aed68013a3a794daa7136a8b62796851509
Maxim Belsky <public.belsky@gmail.com>
2019-03-11 00:16:45 +0300

    Add 008-palindrome problem

---
+++ problems/008-palindrome.js
@@ -0,0 +1,19 @@
+/**
+ * Напишите функцию isPalindrome(value) определяющую,
+ * является ли переданная строка палиндромом, то есть строкой,
+ * которая одинаково читается слева направо и справа налево.
+ *
+ * Пример:
+ *
+ * isPalindrome('121') === true
+ * isPalindrome('boob') === true
+ * isPalindrome('true') === false
+ *
+ * @param {string} value
+ * @returns {boolean}
+ */
+function isPalindrome(value) {
+    return undefined;
+}
+
+module.exports = isPalindrome;
