2ae27be46a2bf7d05babea5ac3fa1ffdb090b47f
Maxim Belsky <public.belsky@gmail.com>
2019-03-12 23:08:22 +0300

    Add 016-sort-timestamps problem

---
+++ problems/016-sort-timestamps.js
@@ -0,0 +1,15 @@
+/**
+ * Напишите функцию sortTimestamps(list) сортирующую список временных моментов.
+ *
+ * Пример:
+ *
+ * sortTimestamps(['23:02:59', '02:07:00']) === ['02:07:00', '23:02:59']
+ *
+ * @param {string[]} list массив временных моментов представленных в виде строк в формате 'HH:MM:SS'
+ * @returns {string[]} отсортированный по возрастанию массив временных моментов
+ */
+function sortTimestamps(list) {
+    return undefined;
+}
+
+module.exports = sortTimestamps;
