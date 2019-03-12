2e40735ffdc59fd695bc796c2b1ad65cda4f6170
Maxim Belsky <public.belsky@gmail.com>
2019-03-12 21:52:37 +0300

    Add 013-ip problem

---
+++ problems/__tests__/013.js
@@ -0,0 +1,10 @@
+const isIpValid = require('../013-ip');
+
+test('Тестирование задачи "013-ip"', () => {
+    expect(isIpValid('127.0.0.0')).toBe(true);
+    expect(isIpValid('192.168.0.01')).toBe(true);
+    expect(isIpValid('255.000.255.0255')).toBe(true);
+    expect(isIpValid('192.255.000.255.0255')).toBe(false);
+    expect(isIpValid('256.-128.256.0256')).toBe(false);
+    expect(isIpValid('Hello world')).toBe(false);
+});
