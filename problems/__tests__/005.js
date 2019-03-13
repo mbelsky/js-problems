acbd7cba3d3c116b8ef47995e3afdaa9387995ea
Maxim Belsky <public.belsky@gmail.com>
2019-03-13 23:59:38 +0300

    Extend union test cases

--- problems/__tests__/005.js
+++ problems/__tests__/005.js
@@ -2,6 +2,7 @@ const union = require('../005-union');
 
 test('Тестирование задачи "005-union"', () => {
     expect(union([1, 1, 3, 2, 5], [5, 3, 7, 7])).toEqual([3, 5]);
+    expect(union([5, 3], [5, 3, 7, 7])).toEqual([3, 5]);
     expect(
         union([2, 4, 6, 8, 10, 12, 10, 8, 6, 4, 2], [3, 6, 9, 12, 15, 18])
     ).toEqual([6, 12]);
