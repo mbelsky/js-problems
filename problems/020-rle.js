4dbfc4860be3170bca6f6ca5568237a1b9716fec
Maxim Belsky <public.belsky@gmail.com>
2019-03-13 22:47:56 +0300

    Add 020-rle problem

---
+++ problems/020-rle.js
@@ -0,0 +1,16 @@
+/**
+ * Напишите функцию rle(value) реализующую алгоритм сжатия строки.
+ *
+ * Пример:
+ *
+ * rle('AAABC') === '3ABC'
+ * rle('AAAaaB') === '3A2aB'
+ *
+ * @param {string} value
+ * @returns {string}
+ */
+function rle(value) {
+    return undefined;
+}
+
+module.exports = rle;
