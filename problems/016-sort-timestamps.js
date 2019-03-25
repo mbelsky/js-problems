7a7855aadc2514952a6d27bc95a80b59b40d39aa
Kate Levantsova <kate.levantsova@gmail.com>
2019-03-25 18:24:00 +0300

    Решена шестнадцатая

--- problems/016-sort-timestamps.js
+++ problems/016-sort-timestamps.js
@@ -8,8 +8,22 @@
  * @param {string[]} list массив временных моментов представленных в виде строк в формате 'HH:MM:SS'
  * @returns {string[]} отсортированный по возрастанию массив временных моментов
  */
+
+function HowMath(element) {
+    var arrTime = element.split(":");
+    var sec =
+        Math.abs(arrTime[0]) * Math.pow(60, 2) +
+        Math.abs(arrTime[1]) * 60 +
+        Math.abs(arrTime[2]);
+    return sec;
+}
+
+function compareNumbers(a, b) {
+    return HowMath(a) - HowMath(b);
+}
+
 function sortTimestamps(list) {
-    return undefined;
+    return list.sort(compareNumbers);
 }
 
 module.exports = sortTimestamps;
