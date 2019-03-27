8983e03c3e11545723af099e100269b28e25dd9c
Maxim Belsky <public.belsky@gmail.com>
2019-03-27 23:16:48 +0300

    Extend 018-anagram test cases

--- problems/__tests__/018.js
+++ problems/__tests__/018.js
@@ -7,5 +7,9 @@ test('Тестирование задачи "018-anagram"', () => {
     expect(anagram('апельсин', 'спаниель')).toBe(true);
     expect(anagram('Волосы', 'Слово')).toBe(false);
     expect(anagram('Волос', 'Словоъ')).toBe(false);
+    expect(anagram('Волос', 'Волас')).toBe(false);
+    expect(anagram('Волос', 'Волоз')).toBe(false);
+    expect(anagram('11222', '11122')).toBe(false);
+    expect(anagram('123', '234')).toBe(false);
     expect(anagram('', '')).toBe(false);
 });
