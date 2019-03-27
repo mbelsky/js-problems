d92ca129fe93375363d5679ea1b2b05e74dc1597
Kate Levantsova <kate.levantsova@gmail.com>
2019-03-27 18:47:00 +0300

    Решена задача 19

--- problems/019-search-anagrams.js
+++ problems/019-search-anagrams.js
@@ -11,8 +11,40 @@
  * @param {string} value
  * @returns {string}
  */
+
+function anagram(x, y) {
+    if ((x, y.length == 0 || x.length != y.length)) {
+        return false;
+    }
+
+    for (var i = 0; i < x.length; i++) {
+        // var reg = x[i];
+        var reg = new RegExp(x[i], "i");
+        if (!reg.test(y)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function searchAnagrams(value) {
-    return undefined;
+    if (value.length == 0) return value;
+    var result = [];
+    var arrayWord = value.match(/[a-яА-Я]+/g);
+    for (var i = 0; i < arrayWord.length; i++) {
+        const element = arrayWord[i];
+        var arrayFilter = arrayWord.filter(function(elements) {
+            return elements != element;
+        });
+        for (var j = 0; j < arrayFilter.length; j++) {
+            const elementY = arrayFilter[j];
+
+            if (anagram(element, elementY)) {
+                result.push(element);
+            }
+        }
+    }
+    return result.join(" ");
 }
 
 module.exports = searchAnagrams;
