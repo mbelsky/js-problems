cd3e87c14cb7aa32d043b568f45abfb02d2cf646
Max Belsky <public.belsky@gmail.com>
2019-03-29 11:10:05 +0300

    Extend "006-spring-melt" test cases

--- problems/__tests__/006.js
+++ problems/__tests__/006.js
@@ -3,5 +3,6 @@ const getSpringMeltStreak = require('../006-spring-melt');
 test('Тестирование задачи "006-spring-melt"', () => {
     expect(getSpringMeltStreak([-20, 30, -40, 50, 10, -10])).toBe(2);
     expect(getSpringMeltStreak([10, 20, 30, 1, -10, 1, 2, 3])).toBe(4);
+    expect(getSpringMeltStreak([10, 20, 30, 0, -10, 5, 8, 1, 2, 3])).toBe(5);
     expect(getSpringMeltStreak([-10, 0, -10, 0, -10])).toBe(0);
 });
