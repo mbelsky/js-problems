7a40042629d86a86294170704df4285eeea81ab4
Maxim Belsky <public.belsky@gmail.com>
2019-04-09 23:00:59 +0300

    Add 027-has problem

---
+++ problems/027-has.js
@@ -0,0 +1,19 @@
+/**
+ * Напишите функцию has(path, object) возвращающую true, если в объекте есть свойство
+ * описанное массивом path, иначе false
+ *
+ * Пример:
+ *
+ * has(['a'], { a: 1 }) === true
+ * has(['b'], { a: 1 }) === false
+ * has(['o', 'a'], { o: { a: 2 } }) === true
+ *
+ * @param {string[]} path
+ * @param {object} object
+ * @returns {boolean}
+ */
+function has(path, object) {
+    return undefined;
+}
+
+module.exports = has;
---
+++ problems/__tests__/027.js
@@ -0,0 +1,31 @@
+const has = require('../027-has');
+
+const testObject = {
+    a: 1,
+    o: {
+        a: 2
+    },
+    n: null,
+    'a.b.c': undefined
+};
+
+test('Тестирование задачи "027-has"', () => {
+    expect(has(['a'], testObject)).toBe(true);
+    expect(has(['b'], testObject)).toBe(false);
+    expect(has(['o', 'a'], testObject)).toBe(true);
+    expect(has(['o', 'b'], testObject)).toBe(false);
+    expect(has(['a.b.c'], testObject)).toBe(true);
+    expect(has(['n'], testObject)).toBe(true);
+
+    expect(has(['n'], {})).toBe(false);
+    expect(has(['n'], null)).toBe(false);
+    expect(has(['n'], undefined)).toBe(false);
+
+    function Foo() {}
+    Foo.prototype.a = 1;
+    // false для наследуемых свойств
+    expect(has(['a'], new Foo())).toBe(false);
+    Foo.prototype.hasOwnProperty = null;
+    // Должен использоваться hasOwnProperty из Object.prototype с привязкой контекста
+    expect(has(['a'], new Foo())).toBe(false);
+});
