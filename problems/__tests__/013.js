656d742d5dbbc9689de97f83cbaea55cdc7960c4
Maxim Belsky <public.belsky@gmail.com>
2019-05-08 20:42:47 +0300

    Extend 013-ip tests

--- problems/__tests__/013.js
+++ problems/__tests__/013.js
@@ -5,6 +5,7 @@ test('Тестирование задачи "013-ip"', () => {
     expect(isIpValid('192.168.0.01')).toBe(true);
     expect(isIpValid('255.000.255.0255')).toBe(true);
     expect(isIpValid('192.255.000.255.0255')).toBe(false);
+    expect(isIpValid('127.-127.0.0')).toBe(false);
     expect(isIpValid('256.-128.256.0256')).toBe(false);
     expect(isIpValid('Hello world')).toBe(false);
 });
