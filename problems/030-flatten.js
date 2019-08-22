46d919453d8779a31cb78b414ddfc9cdbf8bce8c
Maxim Belsky <public.belsky@gmail.com>
2019-08-22 23:47:56 +0300

    Fix JsDoc

--- problems/030-flatten.js
+++ problems/030-flatten.js
@@ -6,8 +6,8 @@
  * flatten([1, [2, 3]]) === [1, 2, 3]
  * flatten([1, [2, [3, 4]]]) === [1, 2, 3, 4]
  *
- * @param {}
- * @returns {}
+ * @param {Array.<number|[]>} array
+ * @returns {number[]}
  */
 function flatten(array) {
     return undefined;
